909bc7e9251e95ef79497d3f662d04cf4fdcbd4d
Jose Luis Encarnacion <jose.lencarnacion1@gmail.com>
2020-11-15 19:00:49 -0800

    working with media querys

--- gulpfile.js
+++ gulpfile.js
@@ -8,64 +8,27 @@ const browsersync = require('browser-sync')
 
 const server = browsersync.create();
 
+//sass
 
-
-//  task
-
-gulp.task('pug', () => {
-    gulp.src('./dev/pug/pages/**/*.pug')
-    .pipe(pug())
-    .pipe(gulp.dest('./public/'))
-});
-
-
-// Sass
-
-gulp.task('sass', () => {
-    gulp.src('./dev/sass/**/*main.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('public/main css/'))
-    .pipe(browsersync.stream())
-
-})
-
-
-// imagesmin
-
-gulp.task('imagemin', () => 
-gulp.src('./dev/images/**/*')
-.pipe(plumber())
-.pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-    imagemin.mozjpeg({quality: 75, progressive: true}),
-    imagemin.optipng({optimizationLevel: 5}),
-    imagemin.svgo({
-        plugins: [
-            {removeViewBox: true},
-            {cleanupIDs: false}
-        ]
-    })
-]))
-.pipe(gulp.dest('./public/images'))
+gulp.task('sass', () => 
+gulp.src('./dev/sass/main.scss')
+.pipe(sass())
+.pipe(gulp.dest('./public/'))
 );
 
+// html
+gulp.task('html', () =>
+gulp.src('./public/*.html')
 
-gulp.task('html', () => 
-gulp.src('./public/**/*html')
-)
-
-// server
+);
 
 gulp.task('server', () => {
     server.init({
-    server: {
-        baseDir: "./public"
-    }
+        server: {
+            baseDir: './public/'
+        }
+    })
+
+    gulp.watch('./dev/sass/**/*.scss', gulp.series('sass')).on('change', server.reload);
+    gulp.watch('./public/**/*.html', gulp.series('html')).on('change', server.reload)
 })
-
-
-gulp.watch('./dev/sass/**/*.scss', gulp.series('sass')).on('change', server.reload);
-gulp.watch('./public/**/*.html', gulp.series('html')).on('change', server.reload);
-
-
-});
